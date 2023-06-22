edf7c9b01627ebea1f4f02b19da7f09ceca80a5c
Marcus Melo <marcus.melo@dcomp.ufs.br>
2023-06-22 01:58:49 -0300

    iniciado css e html da section de cedulas e moedas

--- frontEnd/src/pages/cedulas-moedas/index.tsx
+++ frontEnd/src/pages/cedulas-moedas/index.tsx
@@ -12,6 +12,23 @@ function Main() {
         <Aside />
         <section>
           <h2>Pagina de cédulas e moedas</h2>
+          <div className="container cedulas-moedas">
+            <div className="column cedulas-moedas">
+              <div className="content">1</div>
+              <div className="content">2</div>
+              <div className="content">3</div>
+              <div className="content">4</div>
+            </div>
+
+            <div className="column cedulas-moedas">
+              <div className="content">5</div>
+              <div className="content">6</div>
+              <div className="content">7</div>
+              <div className="content">8</div>
+            </div>
+          </div>
+        
+
         </section>
       </main>
       <Footer />

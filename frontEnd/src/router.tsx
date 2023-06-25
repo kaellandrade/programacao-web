9fc5f6da9638f8ace639a765da94cc6e510f9b77
Micael Andrade Dos Santos <micaelandrade@moobitech.com.br>
2023-06-25 12:41:59 -0300

    Configurando lib de roteamento.

---
+++ frontEnd/src/router.tsx
@@ -0,0 +1,30 @@
+import {
+    createBrowserRouter,
+} from 'react-router-dom';
+import EvolucaoTemporal from './pages/Evolucao_temporal';
+import Root from './pages/pageRoot';
+
+import Valores from './pages/valores';
+
+import Pagina404 from './pages/page404';
+
+const router = createBrowserRouter([
+	{
+	  path: '/',
+	  element: 	< Root/>,
+	  errorElement: <Pagina404 />,
+	  children:[
+
+			{
+			  path: 'valores',
+			  element: <Valores/>,
+			},
+			{
+				path: 'evolucao',
+				element: <EvolucaoTemporal/>,
+			}
+	  ]
+	},
+  ]);
+
+export default router;

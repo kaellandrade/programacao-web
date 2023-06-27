00b39363f9df3110960b2a32c7c458f547793ef5
Marcus Melo <marcus.melo@dcomp.ufs.br>
2023-06-27 15:09:51 -0300

    funcionalidade de obter valor extenso

--- backEnd/nodejs/src/index.ts
+++ backEnd/nodejs/src/index.ts
@@ -1,11 +1,14 @@
 import express from "express";
 import { AppDataSource } from "./data-source";
 import routes from "./routes";
+const cors = require('cors');
 
 AppDataSource.initialize().then(() => {
   const app = express();
 
   app.use(express.json());
+
+  app.use(cors());
 
   app.use(routes);
 
---
+++ frontEnd/src/componentes/cardValores/Calendar.tsx
@@ -0,0 +1,43 @@
+import React, { useState } from 'react';
+import { Calendar, CalendarChangeEvent } from 'primereact/calendar';
+import { addLocale } from 'primereact/api';
+
+export default function MinMaxDemo() {
+
+    const [date, setDate] = useState<string | Date | Date[] | null>(null);
+
+    const minDate = new Date();
+    minDate.setMonth(8);
+    minDate.setFullYear(1994);
+
+    const maxDate = new Date();
+    maxDate.setMonth(6);
+    maxDate.setFullYear(2023);
+
+    addLocale('pt-br', {
+        firstDayOfWeek: 1,
+        dayNames: ['domingo', 'segunda', 'terça', 'quarta', 'quinta', 'sexta', 'sábado'],
+        dayNamesShort: ['dom', 'seg', 'ter', 'qua', 'qui', 'sex', 'sáb'],
+        dayNamesMin: ['D', 'S', 'T', 'Q', 'Q', 'S', 'S'],
+        monthNames: ['janeiro', 'fevereiro', 'março', 'abril', 'maio', 'junho', 'julho', 'agosto', 'setembro', 'outubro', 'novembro', 'dezembro'],
+        monthNamesShort: ['jan', 'fev', 'mar', 'abr', 'mai', 'jun', 'jul', 'ago', 'set', 'out', 'nov', 'dez'],
+        today: 'Hoje',
+        clear: 'Limpar'
+    });
+
+    return (
+        <div className="card flex justify-content-center">
+            <Calendar 
+            value={date} 
+            onChange={(e: CalendarChangeEvent) => setDate(e.value)} 
+            minDate={minDate} 
+            maxDate={maxDate} 
+            locale="pt-br"
+            placeholder="Selecione uma data"
+            showIcon
+            dateFormat="dd/mm/yy"
+            readOnlyInput />
+        </div>
+    );
+}
+        

c07b86b27aa22102c5a515bf5c95296b95efd775
Marcus Melo <marcus.melo@dcomp.ufs.br>
2023-06-28 16:55:44 -0300

    page Valores integrada com back end

--- frontEnd/src/api/data.ts
+++ frontEnd/src/api/data.ts
@@ -1,15 +1,53 @@
 import axios from 'axios';
+const urlBackEnd = 'http://localhost:3000';
 
-export default function getValorExtenso(valor: number): Promise<string> {
+export function getValorExtenso(valor: number): Promise<string> {
 	return new Promise<string>((resolve, reject) => {
-	  axios.get(`http://localhost:3000/valores/${valor}`)
-		.then((response) => {
-		  resolve(response.data.valorExtensoMaiuscula);
-		})
-		.catch((error) => {
-		  console.error(error);
-		  reject('Erro ao obter o valor por extenso.');
-		});
+		if (valor != undefined) {
+			axios.get(`${urlBackEnd}/valores/${valor}`)
+			.then((response) => {
+				resolve(response.data.valorExtensoMaiuscula);
+			})
+			.catch((error) => {
+				console.error(error);
+				reject('Erro ao obter o valor por extenso.');
+			});
+		}
 	});
-  }
+}
+
+export function getValorCirculacaoDataEspecifica(data: string, especie: string): Promise<string> {
+	// console.log(data, especie);
+	return new Promise<string>((resolve, reject) => {
+		if (data != undefined && especie != undefined) {
+			axios.get(`${urlBackEnd}/valorCirculacaoDataEspecifica/${data}/${especie}`)
+			.then((response) => {
+				// console.log(response.data[0].valor_total);	
+			  resolve(response.data[0].valor_total);
+			})
+			.catch((error) => {
+				console.error(error);
+				reject('Erro ao obter o valor por extenso.');
+			});
+		}
+		else if ( data === 'undefined-undefined-' )
+			console.log('data undefined');
+	});
+}
   
+export function getValorCirculacaoIntervaloAnos(anoInicio: { year: number }, anoFim: { year: number }, especie: string): Promise<string> {
+	console.log(anoInicio.year, anoFim.year, especie);
+	return new Promise<string>((resolve, reject) => {
+		if ( anoInicio != undefined && anoFim != undefined && especie != undefined) {
+			axios.get(`${urlBackEnd}/valorCirculacaoIntervaloAnos/${anoInicio.year}/${anoFim.year}/${especie}`)
+			.then((response) => {
+			//   console.log(response.data[0].valor_total);	
+				resolve(response.data[0].valor_total);
+			})
+			.catch((error) => {
+				console.error(error);
+				reject('Erro ao obter o valor por extenso.');
+			});
+		}
+	});
+}
--- frontEnd/src/pages/pageValores/index.tsx
+++ frontEnd/src/pages/pageValores/index.tsx
@@ -1,32 +1,29 @@
 import './index.css';
 import Card_Valores from '../../componentes/cardValores/CardValores';
 
-
 function Main() {
 
   const pergunta1 = 'Qual é o valor em circulação somente das moedas em uma data específica?';
-  const valor_moeda_data = 151000000.12;
-
   const pergunta2 = 'Qual é o valor em circulação somente das moedas em um intervalo de anos?';
-  const valor_moeda_ano = 1203333223.05;
-
   const pergunta3 = 'Qual é o valor em circulação somente das cédulas em uma data específica?';
-  const valor_cedula_data = 82151000000.12;
-  
   const pergunta4 = 'Qual é o valor em circulação somente das cédulas em um intervalo de anos?';
-  const valor_cedula_ano = 49203333223.05;
+
+  const calendarioId1 = 'calendario-input-1';
+  const calendarioId2 = 'calendario-input-2';
+  const calendarioId3 = 'calendario-input-3';
+  const calendarioId4 = 'calendario-input-4';
 
   return (
     <>
       <h2>Pagina de valores</h2>
       <div className="container valores">
         <div className="column valores">
-          <Card_Valores pergunta={pergunta1} valor_moeda_data={valor_moeda_data} possuiIntervalo={false} />
-          <Card_Valores pergunta={pergunta2} valor_moeda_data={valor_moeda_ano} possuiIntervalo={true} />
+          <Card_Valores pergunta={pergunta1} isMoeda={true} possuiIntervalo={false} calendarioId={calendarioId1} />
+          <Card_Valores pergunta={pergunta2} isMoeda={true} possuiIntervalo={true} calendarioId={calendarioId2} />
         </div>
         <div className="column valores">
-          <Card_Valores pergunta={pergunta3} valor_moeda_data={valor_cedula_data} possuiIntervalo={false} />
-          <Card_Valores pergunta={pergunta4} valor_moeda_data={valor_cedula_ano} possuiIntervalo={true} />
+          <Card_Valores pergunta={pergunta3} isMoeda={false} possuiIntervalo={false} calendarioId={calendarioId3} />
+          <Card_Valores pergunta={pergunta4} isMoeda={false} possuiIntervalo={true} calendarioId={calendarioId4} />
         </div>
       </div>
     </>

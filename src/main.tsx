fb7f8fa932bdb03a18f913d7c294a17e5a8af1fc
Gabriel Silva de Jesus <gabriel.jesus@sergipetec.org.br>
2023-07-22 18:20:45 -0300

    chore: configuracoes do analytics

--- src/main.tsx
+++ src/main.tsx
@@ -10,35 +10,51 @@ import PageQuantidade from './pages/pageQuantidade';
 import PageRanking from './pages/pageRanking';
 import PageValores from './pages/pageValores';
 import PageEvoucaoTemporal from './pages/pageEvolucaoTemporal';
+import reportWebVitals from './reportWebVitals';
+
+import ReactGA from 'react-ga4';
+
+ReactGA.initialize('G-HRSML86GPK');
 
 const router = createBrowserRouter([
 	{
-	  path: '/',
-	  element: 	<PageRoot />,
-	  errorElement: <Page404 />,
-	  children:[
+		path: '/',
+		element: <PageRoot />,
+		errorElement: <Page404 />,
+		children: [
 			{
-			  path: 'valores',
-			  element: <PageValores />,
+				path: 'valores',
+				element: <PageValores />
 			},
 			{
 				path: 'evolucao',
-				element: <PageEvoucaoTemporal />,
+				element: <PageEvoucaoTemporal />
 			},
 			{
 				path: 'ranking',
-				element: <PageRanking />,
+				element: <PageRanking />
 			},
 			{
 				path: 'quantidade',
-				element: <PageQuantidade />,
+				element: <PageQuantidade />
 			}
-	  ]
+		]
 	}
-  ]);
+]);
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
 	<React.StrictMode>
 		<RouterProvider router={router} />
 	</React.StrictMode>
 );
+
+const SendAnalytics = () => {
+	ReactGA.send({
+		hitType: 'pageview',
+		page: window.location.pathname
+	});
+};
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals(SendAnalytics);

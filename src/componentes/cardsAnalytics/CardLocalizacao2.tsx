927be039c2baca95389906092d8ee23955a2c501
Marcus Melo <marcus.melo@dcomp.ufs.br>
2023-08-09 00:14:00 -0300

    leaflet funcionando

--- src/componentes/cardsAnalytics/CardLocalizacao2.tsx
+++ src/componentes/cardsAnalytics/CardLocalizacao2.tsx
@@ -1,5 +1,6 @@
 import { useState, useEffect } from 'react';
-import { MapContainer, TileLayer, Popup, Marker, Circle } from 'react-leaflet';
+import { DataTable } from 'primereact/datatable';
+import { Column } from 'primereact/column';
 
 function CardLocalizacao() {
 
@@ -36,22 +37,11 @@ function CardLocalizacao() {
   return (
     <div className="content">
       <h4>Localização</h4>
-      <MapContainer style={{ height: '100vh', width: '100wh' }} center={[-10.9095, -37.0748]} zoom={3} scrollWheelZoom={true}>
-      <TileLayer
-        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-      />
-      {/* <Marker position={[-10.9095, -37.0748]}>
-        <Popup>
-          A pretty CSS3 popup. <br /> Easily customizable.
-        </Popup>
-      </Marker> */}
-      <Circle
-        center={[-10.9095, -37.0748]}
-        
-        radius={10000}
-      />
-    </MapContainer>
+      <DataTable size="small" value={dados} scrollable scrollHeight="100%" sortMode="multiple" tableStyle={{ minWidth: '20rem' }} id='tableQuantidade' >
+        {colunas.map((col: any) => (
+            <Column key={col.field} field={col.field} header={col.header} align="center" sortable style={{ width: '10%' }} />
+        ))}
+      </DataTable>
     </div>
       
   );
